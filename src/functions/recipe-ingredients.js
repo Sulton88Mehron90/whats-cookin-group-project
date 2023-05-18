837fe835e96624b75eba9212445407ecc2ea659a
Parvin Sattorova <nanajon66@gmail.com>
2023-05-18 17:56:58 -0600

    refactor: Refactor recipeIngredients function

--- src/functions/recipe-ingredients.js
+++ src/functions/recipe-ingredients.js
@@ -4,24 +4,23 @@ import ingredientsData from "../data/ingredients";
 import recipeData from "../data/recipes";
 
 const recipeIngredients = (recipe) => {
-  const filteredRecipe = recipeData.find((item) => {
-   return item.name === recipe
-  });
-  if (!filteredRecipe){
-    return `Sorry, cannot find a recipe for ${recipe}.`
+  const filteredRecipe = recipeData.find(item => item.name === recipe);
+
+  if (!filteredRecipe) {
+    return `Sorry, cannot find a recipe for ${recipe}.`;
   }
-  const ingredientIds = filteredRecipe.ingredients.map((ingredient) => {
-    return ingredient.id
-  })
-  let ingredientObjects = ingredientIds.map((ingedientId) => {
-    return ingredientsData.find((ingredient) => {
-      if(ingredient.id === ingedientId) {
-        return ingredient.name
-      }
-    })
-  })
-  let ingredientStrings = ingredientObjects.map((object) => object.name)
-  return ingredientStrings
-}
+
+  const ingredientIds = filteredRecipe.ingredients.map(ingredient => ingredient.id);
+
+  const ingredientObjects = ingredientIds.map(ingredientId => {
+    return ingredientsData.find(ingredient => ingredient.id === ingredientId);
+  });
+
+  const ingredientStrings = ingredientObjects.map(object => object.name);
+
+  return ingredientStrings;
+};
+
+
 
 export { recipeIngredients };
