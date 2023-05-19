81795b855b3c8ac29d57d559d070c3741fac56cf
Marisa Wyatt <risawyatt22@gmail.com>
2023-05-19 17:58:17 -0600

    feat: Style ingredient cost to match

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -71,7 +71,7 @@ const viewRecipe = (recipe) => {
   })
   recipeTitle.innerText = recipe.name;
   imageContainer.innerHTML = `<img style= "margin: 2em;border-radius: 25px" src="${recipe.image}">`;
-  recipe.instructions.forEach((instruction) => instructionsDisplay.innerHTML += `<p style="align-self: flex-start">${instruction.number}.) ${instruction.instruction}</p>`);
+  recipe.instructions.forEach((instruction) => instructionsDisplay.innerHTML += `<p style="align-self: flex-start;margin: 1em">${instruction.number}.) ${instruction.instruction}</p>`);
   recipeCost.innerHTML = `<p>${recipe.cost}</p>`;
 };
 
