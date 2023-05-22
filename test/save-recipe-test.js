3065d3e8bfc509bb780ebd261d67a5c871781cec
Benjamin holden <benjamin.f.holden@outlook.com>
2023-05-21 22:56:22 -0600

    feat: added update main branch to current branch

--- test/save-recipe-test.js
+++ test/save-recipe-test.js
@@ -19,21 +19,3 @@ describe('save recipe', () => {
     expect(savedRecipes.length).to.equal(0)
   })
 })
-
-// import { expect } from 'chai';
-// import { recipeIngredients } from '../src/functions/recipe-ingredients';
-
-// describe('get recipe ingredients', () => {
-//   it('should be a function', function() {
-//     expect(recipeIngredients).to.be.a('function')
-//   })
-//   it('should return the ingredients for a recipe', () => {
-//     const ingredients = recipeIngredients("Maple Dijon Apple Cider Grilled Pork Chops")
-//     expect(ingredients[0]).to.deep.equal({ name: 'apple cider', amount: 1.5, unit: 'cups', cost: 468 })
-//   })
-
-//   it('should return a error message when given an invalid recipe', () => {
-//     expect(recipeIngredients('hotdog')).to.equal('Sorry, cannot find a recipe for hotdog.')
-//   })
-// })
-
