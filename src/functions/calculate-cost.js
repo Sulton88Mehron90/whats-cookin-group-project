6566770d41a0adf6d040df01e2d62772e999c00d
Marisa Wyatt <risawyatt22@gmail.com>
2023-05-22 19:27:45 -0600

    refactor: correct formatting in calculate-cost function

--- src/functions/calculate-cost.js
+++ src/functions/calculate-cost.js
@@ -1,28 +1,24 @@
-// import ingredientsData from "../data/ingredients";
-import { recipeData, ingredientsData } from "../apiCalls"
-
 const calculateCost = (name, recipeData, ingredientsData) => {
   let costs = [];
   const currentRecipe = recipeData.find((item) => {
-    return item.name === name
+    return item.name === name;
   });
   if(!currentRecipe){
-    return `Cannot calculate ${name} recipe cost`
+    return `Cannot calculate ${name} recipe cost`;
   } else {
   let recipeIngredients = currentRecipe.ingredients;
   recipeIngredients.forEach((recipeIngredient) => {
     ingredientsData.forEach((ingredient) => {
       if (ingredient.id === recipeIngredient.id){
-        costs.push(recipeIngredient.quantity.amount * (ingredient.estimatedCostInCents / 100))
+        costs.push(recipeIngredient.quantity.amount * (ingredient.estimatedCostInCents / 100));
       }
-    })
-  })
+    });
+  });
   let totalCost = costs.reduce((acc, currentValue) => 
-  acc + currentValue)
-  totalCost = totalCost.toFixed(2)
-  return `$${totalCost}`
+  acc + currentValue);
+  totalCost = totalCost.toFixed(2);
+  return `$${totalCost}`;
   }
-}
-
+};
 
 export { calculateCost }
