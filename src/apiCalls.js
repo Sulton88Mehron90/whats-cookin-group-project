d816cf9d9ea6f5523e90b361470136f6a54f07b7
Jamie Caudill <j.caudill7177@gmail.com>
2023-06-01 15:53:35 -0700

    refactor: Refactor api calls to fetch from a local server

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,7 @@
 // FETCH REQUESTS //
 
-const fetchRecipes = fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes`)
-const fetchIngredients = fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients`)
-const fetchUsers = fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users`)
+const fetchRecipes = fetch(`http://localhost:3001/api/v1/recipes`)
+const fetchIngredients = fetch(`http://localhost:3001/api/v1/ingredients`)
+const fetchUsers = fetch(`http://localhost:3001/api/v1/users`)
 
 export { fetchRecipes, fetchIngredients, fetchUsers };
